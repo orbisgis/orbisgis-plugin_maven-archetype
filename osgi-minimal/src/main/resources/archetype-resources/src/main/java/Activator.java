7141a9e673a4a15c4293407a04349067f8598bef
nicolas-f <github@nettrader.fr>
2012-12-12 11:46:05 +0100

    Define minimal OSGI Archetype

---
+++ osgi-minimal/src/main/resources/archetype-resources/src/main/java/Activator.java
@@ -0,0 +1,33 @@
+package ${package};
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+/**
+ * Registers services provided by this plugin bundle.
+ */
+public class Activator implements BundleActivator {
+        /**
+         * Starting bundle, register services.
+         * @param bc
+         * @throws Exception
+         */
+        @Override
+        public void start(BundleContext bc) throws Exception {
+                /*
+                bc.registerService(ServiceClass.class.getName(),
+                        new ServiceImplementation(),
+                        null);
+                */
+        }
+
+        /**
+         * Called before the bundle is unloaded.
+         * @param bc
+         * @throws Exception
+         */
+        @Override
+        public void stop(BundleContext bc) throws Exception {
+
+        }
+}
